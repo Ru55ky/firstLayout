475fa6e1080dc53862d2aa003bc99388d91f8542
Михаил <misha-dobatkin@mail.ru>
2019-08-09 17:28:36 +0300

    поправил картинку с лесом

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var btn = document.querySelector('.burger');
-btn.addEventListener('click', function (evt) { 
+btn.addEventListener("click", function (evt) { 
     evt.preventDefault();
     btn.classList.toggle('burger_active');
  });
