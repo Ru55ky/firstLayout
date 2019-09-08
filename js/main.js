387e9de437012aaabe86a3172bec95501a7460ee
Михаил <misha-dobatkin@mail.ru>
2019-09-08 13:05:46 +0300

    Добавлен JS к Popup. Popup теперь можно закрыть через ESC

--- js/main.js
+++ js/main.js
@@ -3,3 +3,18 @@ btn.addEventListener("click", function (evt) {
     evt.preventDefault();
     btn.classList.toggle('burger_active');
  });
+
+
+ /*POPUP*/
+ let Openpopup = document.querySelector('.Subscribe');
+ let popup = document.querySelector('.container-popup');
+ let input = document.querySelector('.popup-input');
+ Openpopup.addEventListener("click", function () { 
+     popup.classList.add('popup-close');
+
+  });
+document.addEventListener("keydown", function(evt){
+    if(evt.keyCode === 27){
+        popup.classList.remove('popup-close');
+    }
+});
